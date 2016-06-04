2e9d9dc93329522ee7100570eb4cdb8a04ef255d
lokeshwrn <lokeshwrn27@gmail.com>
2016-06-04 14:07:46 +0530

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
 
   def dashboard
     @page_properties={:header => "Dashboard"}
-    @category = Category.by_active.top_by_count.last(3)
-    @sub_category = SubCategory.by_active.top_by_count.last(3)
-    @articles = Article.by_active.top_rated.limit(3)
+    @category = Category.by_active.top_by_count.first(3)
+    @sub_category = SubCategory.by_active.top_by_count.first(3)
+    @articles = Article.by_active.top_rated.first(3)
   end
 
   def tiny_url
